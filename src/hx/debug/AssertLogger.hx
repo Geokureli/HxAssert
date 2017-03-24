74326f8220fe0bb537a917b1c6674aab2dde7583
GeoKureli <Gkurelic@gmail.com>
2017-03-23 23:11:55 -0700

    better multi instance handling

--- src/hx/debug/AssertLogger.hx
+++ src/hx/debug/AssertLogger.hx
@@ -7,186 +7,186 @@ import haxe.PosInfos;
  * @author George
  */
 class AssertLogger {
-
-	public function new(fail:?String->?PosInfos->Void):Void { 
-		
-		if (fail != null)
-			this.fail = fail;
-	}
-	
-	/**
-	 * Called by failed asserts, throws an error by default. 
-	 * Can be redirected to any function.
-	 * 
-	 * @example myAssert.fail = haxe.Log.trace;    myAssert.isTrue(false, "test");// output: test
-	 * 
-	 * @param msg  An optional error message. If not passed a default one will be used
-	 */
-	public dynamic function fail(?msg:String, ?pos:PosInfos):Void {
-		
-		if (msg == null) msg = "Failure";
-		
-		throw '${pos.fileName}:${pos.lineNumber}: $msg';
-	}
-	
-	inline public function isTrue(cond:Bool, ?msg:String, ?pos:PosInfos):Bool {
-		
-		if (!cond) fail(msg, pos);
-		
-		return cond;
-	}
-	
-	inline public function isFalse(cond:Bool, ?msg:String, ?pos:PosInfos):Bool {
-		
-		return isTrue(!cond, msg, pos);
-	}
-	
-	inline public function isNull(value:Dynamic, ?msg:String, ?pos:PosInfos):Bool {
-		
-		if (msg == null) msg = 'Expected null, got ${q(value)}';
-		
-		return isTrue(value == null, msg, pos);
-	}
-	
-	inline public function nonNull(value:Dynamic, ?msg:String, ?pos:PosInfos):Bool {
-		
-		if (msg == null) msg = 'Unexpected null';
-		
-		return isTrue(value != null, msg, pos);
-	}
-	
-	
-	inline public function has(value:Dynamic, property:String, ?msg:String, ?pos:PosInfos):Bool {
-		
-		if (msg == null)
-			msg = 'Could not find property $property';
-		
-		return isTrue(Reflect.hasField(value, property), msg, pos);
-	}
-	
-	inline public function is(value:Dynamic, type:Dynamic, ?msg:String, ?pos:PosInfos):Bool {
-		
-		if (msg == null)
-			msg = 'Expected type ${typeToString(type)} but it is ${typeToString(value)}';
-		
-		return isTrue(Std.is(value, type), msg, pos);
-	}
-	
-	inline public function isNot(value:Dynamic, type:Dynamic, ?msg:String, ?pos:PosInfos):Bool {
-		
-		if (msg == null)
-			msg = 'Unexpected type ${typeToString(type)}';
-		
-		return isTrue(!Std.is(value, type), msg, pos);
-	}
-	
-	inline public function isObject(value:Dynamic, ?msg:String, ?pos:PosInfos):Bool {
-		
-		if (msg == null)
-			msg = "Expected Object type";
-		
-		return isTrue(Reflect.isObject(value), msg, pos);
-	}
-	
-	inline public function equals(value:Dynamic, expected:Dynamic, ?msg:String, ?pos:PosInfos):Bool {
-		
-		if(msg == null) msg = 'Expected ${q(expected)} but it is ${q(value)}';
-		
-		return isTrue(expected == value, msg, pos);
-	}
-	
-	inline public function notEquals(value:Dynamic, expected:Dynamic, ?msg:String, ?pos:PosInfos):Bool {
-		
-		if (msg == null)
-			msg = 'Expected ${q(expected)} and test value ${q(value)} should be different';
-		
-		return isTrue(value != expected, msg, pos);
-	}
-	
-	inline public function match(pattern:EReg, value:Dynamic, ?msg:String, ?pos:PosInfos):Bool {
-		
-		if(msg == null) msg = '${q(value)} does not match the provided pattern';
-		
-		return isTrue(pattern.match(value), msg, pos);
-	}
-	
-	inline public function floatEquals(value:Float, expected:Float, approx:Float, ?msg:String, ?pos:PosInfos):Bool {
-		
-		if (msg == null) msg = 'Expected ${q(expected)} but it is ${q(value)}';
-		
-		return isTrue(compareFloats(expected, value, approx), msg, pos);
-	}
-	
-	function compareFloats(value:Float, expected:Float = 0, approx:Float):Bool {
-		
-		if (Math.isNaN(expected)) return Math.isNaN(value);
-		if (Math.isNaN(value))    return false;
-		
-		if (!Math.isFinite(expected) && !Math.isFinite(value))
-			return (expected > 0) == (value > 0);
-		
-		return Math.abs(value - expected) <= approx;
-	}
-	
-	inline public function contains<T>(list:Array<T>, item:T, ?msg:String, ?pos:PosInfos):Bool {
-		
-		if (msg == null) msg = 'Couldn\'t find ${item} in ${q(list)}';
-		return isTrue(Lambda.has(list, item), msg, pos);
-	}
-	
-	inline public function notContains<T>(list:Array<T>, item:T, ?msg:String, ?pos:PosInfos):Bool {
-		
-		if (msg == null) msg = 'Found unexpected ${item} in ${q(list)}';
-		
-		return isTrue(!Lambda.has(list, item), msg, pos);
-	}
-	
-	inline public function stringContains(str:String, token:String, ?msg:String, ?pos:PosInfos):Bool {
-		
-		if (msg == null) msg = 'String ${q(str)} do not contain ${token}';
-		
-		return isTrue(str != null && str.indexOf(token) >= 0, msg, pos);
-	}
-	
-	// =============================================================================
-	//{ region                            HELPERS
-	// =============================================================================
-	
-	function typeToString(t:Dynamic):String {
-		
-		try {
-			var _t = Type.getClass(t);
-			
-			if (_t != null) t = _t;
-			
-		} catch(e:Dynamic) { }
-		
-		try return Type.getClassName(t) catch (e:Dynamic) { }
-		
-		try {
-			var _t = Type.getEnum(t);
-			
-			if (_t != null) t = _t;
-			
-		} catch(e:Dynamic) { }
-		
-		try return Type.getEnumName(t)        catch (e:Dynamic) { }
-		try return Std.string(Type.typeof(t)) catch (e:Dynamic) { }
-		try return Std.string(t)              catch (e:Dynamic) { }
-		
-		return '<unable to retrieve type name>';
-	}
-	
-	/** Wraps in quotes */
-	inline function q(v:Dynamic):String {
-		
-		if (Std.is(v, String))
-			v = '"' + StringTools.replace(v, '"', '\\"') + '"';
-		
-		return Std.string(v);
-	}
-	
-	//} endregion                         HELPERS
-	// =============================================================================
-	
+    
+    public function new(fail:?String->?PosInfos->Void):Void { 
+        
+        if (fail != null)
+            this.fail = fail;
+    }
+    
+    /**
+     * Called by failed asserts, throws an error by default. 
+     * Can be redirected to any function.
+     * 
+     * @example myAssert.fail = haxe.Log.trace;    myAssert.isTrue(false, "test");// output: test
+     * 
+     * @param msg  An optional error message. If not passed a default one will be used
+     */
+    public dynamic function fail(?msg:String, ?pos:PosInfos):Void {
+        
+        if (msg == null) msg = "Failure";
+        
+        throw '${pos.fileName}[${pos.lineNumber}]: $msg';
+    }
+    
+    inline public function isTrue(cond:Bool, ?msg:String, ?pos:PosInfos):Bool {
+        
+        if (!cond) fail(msg, pos);
+        
+        return cond;
+    }
+    
+    inline public function isFalse(cond:Bool, ?msg:String, ?pos:PosInfos):Bool {
+        
+        return isTrue(!cond, msg, pos);
+    }
+    
+    inline public function isNull(value:Dynamic, ?msg:String, ?pos:PosInfos):Bool {
+        
+        if (msg == null) msg = 'Expected null, got ${q(value)}';
+        
+        return isTrue(value == null, msg, pos);
+    }
+    
+    inline public function nonNull(value:Dynamic, ?msg:String, ?pos:PosInfos):Bool {
+        
+        if (msg == null) msg = 'Unexpected null';
+        
+        return isTrue(value != null, msg, pos);
+    }
+    
+    
+    inline public function has(value:Dynamic, property:String, ?msg:String, ?pos:PosInfos):Bool {
+        
+        if (msg == null)
+            msg = 'Could not find property $property';
+        
+        return isTrue(Reflect.hasField(value, property), msg, pos);
+    }
+    
+    inline public function is(value:Dynamic, type:Dynamic, ?msg:String, ?pos:PosInfos):Bool {
+        
+        if (msg == null)
+            msg = 'Expected type ${typeToString(type)} but it is ${typeToString(value)}';
+        
+        return isTrue(Std.is(value, type), msg, pos);
+    }
+    
+    inline public function isNot(value:Dynamic, type:Dynamic, ?msg:String, ?pos:PosInfos):Bool {
+        
+        if (msg == null)
+            msg = 'Unexpected type ${typeToString(type)}';
+        
+        return isTrue(!Std.is(value, type), msg, pos);
+    }
+    
+    inline public function isObject(value:Dynamic, ?msg:String, ?pos:PosInfos):Bool {
+        
+        if (msg == null)
+            msg = "Expected Object type";
+        
+        return isTrue(Reflect.isObject(value), msg, pos);
+    }
+    
+    inline public function equals(value:Dynamic, expected:Dynamic, ?msg:String, ?pos:PosInfos):Bool {
+        
+        if(msg == null) msg = 'Expected ${q(expected)} but it is ${q(value)}';
+        
+        return isTrue(expected == value, msg, pos);
+    }
+    
+    inline public function notEquals(value:Dynamic, expected:Dynamic, ?msg:String, ?pos:PosInfos):Bool {
+        
+        if (msg == null)
+            msg = 'Expected ${q(expected)} and test value ${q(value)} should be different';
+        
+        return isTrue(value != expected, msg, pos);
+    }
+    
+    inline public function match(pattern:EReg, value:Dynamic, ?msg:String, ?pos:PosInfos):Bool {
+        
+        if(msg == null) msg = '${q(value)} does not match the provided pattern';
+        
+        return isTrue(pattern.match(value), msg, pos);
+    }
+    
+    inline public function floatEquals(value:Float, expected:Float, approx:Float = 1e-5, ?msg:String, ?pos:PosInfos):Bool {
+        
+        if (msg == null) msg = 'Expected ${q(expected)} but it is ${q(value)}';
+        
+        return isTrue(compareFloats(expected, value, approx), msg, pos);
+    }
+    
+    function compareFloats(value:Float, expected:Float, approx:Float):Bool {
+        
+        if (Math.isNaN(expected)) return Math.isNaN(value);
+        if (Math.isNaN(value))    return false;
+        
+        if (!Math.isFinite(expected) && !Math.isFinite(value))
+            return (expected > 0) == (value > 0);
+        
+        return Math.abs(value - expected) <= approx;
+    }
+    
+    inline public function contains<T>(list:Iterable<T>, item:T, ?msg:String, ?pos:PosInfos):Bool {
+        
+        if (msg == null) msg = 'Couldn\'t find ${item} in ${q(list)}';
+        return isTrue(Lambda.has(list, item), msg, pos);
+    }
+    
+    inline public function notContains<T>(list:Array<T>, item:T, ?msg:String, ?pos:PosInfos):Bool {
+        
+        if (msg == null) msg = 'Found unexpected ${item} in ${q(list)}';
+        
+        return isTrue(!Lambda.has(list, item), msg, pos);
+    }
+    
+    inline public function stringContains(str:String, token:String, ?msg:String, ?pos:PosInfos):Bool {
+        
+        if (msg == null) msg = 'String ${q(str)} do not contain ${token}';
+        
+        return isTrue(str != null && str.indexOf(token) >= 0, msg, pos);
+    }
+    
+    // =============================================================================
+    //{ region                            HELPERS
+    // =============================================================================
+    
+    function typeToString(t:Dynamic):String {
+        
+        try {
+            var _t = Type.getClass(t);
+            
+            if (_t != null) t = _t;
+            
+        } catch(e:Dynamic) { }
+        
+        try return Type.getClassName(t) catch (e:Dynamic) { }
+        
+        try {
+            var _t = Type.getEnum(t);
+            
+            if (_t != null) t = _t;
+            
+        } catch(e:Dynamic) { }
+        
+        try return Type.getEnumName(t)        catch (e:Dynamic) { }
+        try return Std.string(Type.typeof(t)) catch (e:Dynamic) { }
+        try return Std.string(t)              catch (e:Dynamic) { }
+        
+        return "<Unknown Type>";
+    }
+    
+    /** Wraps in quotes */
+    inline function q(v:Dynamic):String {
+        
+        if (Std.is(v, String))
+            v = '"' + StringTools.replace(v, '"', '\\"') + '"';
+        
+        return Std.string(v);
+    }
+    
+    //} endregion                         HELPERS
+    // =============================================================================
+    
 }
--- src/hx/debug/Expect.hx
+++ src/hx/debug/Expect.hx
@@ -24,164 +24,164 @@ import haxe.PosInfos;
  * @author George
  */
 class Expect {
-	
-	/**
-	 * Called by failed asserts, throws an error by default. 
-	 * Can be redirected to any function.
-	 * 
-	 * @example Assert.fail = haxe.Log.trace;    Assert.isTrue(false, "test");// output: test
-	 * 
-	 * @param msg  An optional error message. If not passed a default one will be used
-	 */
-	static public dynamic function fail(msg = "failure expected", ?pos:PosInfos):Void {
-		
-		trace(msg, pos);
-	}
-	
-	static var _instance:Expect = new Assert();
-	
-	/** Asserts successfully when the condition is true. */
-	inline static public function isTrue(cond:Bool, msg = "Expected true", ?pos:PosInfos):Bool {
-		
-		return _instance.isTrue(cond, msg, pos); 
-	}
-	
-	/** Asserts successfully when the condition is false. */
-	inline static public function isFalse(cond:Bool, msg = "Expected false", ?pos:PosInfos):Bool {
-		
-		return _instance.isFalse(cond, msg, pos);
-	}
-	
-	/** Asserts successfully when the value is null. */
-	inline static public function isNull(value:Dynamic, ?msg:String, ?pos:PosInfos):Bool {
-		
-		return _instance.isNull(value, msg, pos);
-	}
-	
-	/** Asserts successfully when the value is not null. */
-	inline static public function nonNull(value:Dynamic, msg = "Unexpected null", ?pos:PosInfos):Bool {
-		
-		return _instance.nonNull(value, msg, pos);
-	}
-	
-	/**
-	 * Asserts successfully when the 'value' parameter is of the of the passed type 'type'.
-	 * @param value     The parent value to test
-	 * @param property  The property to assert
-	 */
-	inline static public function has(value:Dynamic, property:String, ?msg:String, ?pos:PosInfos):Bool {
-		
-		return _instance.has(value, property, msg, pos);
-	}
-	
-	/**
-	 * Asserts successfully when the 'value' parameter is of the of the passed type 'type'.
-	 * @param value  The value to test
-	 * @param type   The type to test against
-	 */
-	inline static public function is(value:Dynamic, type:Dynamic, ?msg:String, ?pos:PosInfos):Bool {
-		
-		return _instance.is(value, type, msg, pos);
-	}
-	
-	/**
-	 * Asserts successfully when the 'value' parameter is of the of the passed type 'type'.
-	 * @param value  The value to test
-	 * @param type   The type to test against
-	 */
-	inline static public function isNot(value:Dynamic, type:Dynamic, ?msg:String, ?pos:PosInfos):Bool {
-		
-		return _instance.isNot(value, type, msg, pos);
-	}
-	
-	/** Asserts successfully when Reflect.isObject(value) is true. */
-	inline static public function isObject(value:Dynamic, ?msg:String, ?pos:PosInfos):Bool {
-		
-		return _instance.isObject(value, msg, pos);
-	}
-	
-	/**
-	 * Asserts successfully when the value parameter is equal to the expected one.
-	 * 
-	 * @example Assert.equals(10, age);
-	 * 
-	 * @param expected  The expected value to check against
-	 * @param value     The value to test
-	 */
-	inline static public function equals(value:Dynamic, expected:Dynamic, ?msg:String, ?pos:PosInfos):Bool {
-		
-		return _instance.equals(value, expected, msg, pos);
-	}
+    
+    /**
+     * Called by failed asserts, throws an error by default. 
+     * Can be redirected to any function.
+     * 
+     * @example Assert.fail = haxe.Log.trace;    Assert.isTrue(false, "test");// output: test
+     * 
+     * @param msg  An optional error message. If not passed a default one will be used
+     */
+    static public dynamic function fail(?msg:String, ?pos:PosInfos):Void {
+        
+        if (msg == null) msg = "failure";
+        
+        trace(msg, pos);
+    }
+    
+    static var _instance:AssertLogger = new AssertLogger(function(?msg:String, ?pos:PosInfos):Void { fail(msg, pos); } );
+    
+    /** Asserts successfully when the condition is true. */
+    inline static public function isTrue(cond:Bool, msg = "Expected true", ?pos:PosInfos):Bool {
+        
+        return _instance.isTrue(cond, msg, pos); 
+    }
+    
+    /** Asserts successfully when the condition is false. */
+    inline static public function isFalse(cond:Bool, msg = "Expected false", ?pos:PosInfos):Bool {
+        
+        return _instance.isFalse(cond, msg, pos);
+    }
+    
+    /** Asserts successfully when the value is null. */
+    inline static public function isNull(value:Dynamic, ?msg:String, ?pos:PosInfos):Bool {
+        
+        return _instance.isNull(value, msg, pos);
+    }
+    
+    /** Asserts successfully when the value is not null. */
+    inline static public function nonNull(value:Dynamic, msg = "Unexpected null", ?pos:PosInfos):Bool {
+        
+        return _instance.nonNull(value, msg, pos);
+    }
+    
+    /**
+     * Asserts successfully when the 'value' parameter is of the of the passed type 'type'.
+     * @param value     The parent value to test
+     * @param property  The property to assert
+     */
+    inline static public function has(value:Dynamic, property:String, ?msg:String, ?pos:PosInfos):Bool {
+        
+        return _instance.has(value, property, msg, pos);
+    }
+    
+    /**
+     * Asserts successfully when the 'value' parameter is of the of the passed type 'type'.
+     * @param value  The value to test
+     * @param type   The type to test against
+     */
+    inline static public function is(value:Dynamic, type:Dynamic, ?msg:String, ?pos:PosInfos):Bool {
+        
+        return _instance.is(value, type, msg, pos);
+    }
+    
+    /**
+     * Asserts successfully when the 'value' parameter is of the of the passed type 'type'.
+     * @param value  The value to test
+     * @param type   The type to test against
+     */
+    inline static public function isNot(value:Dynamic, type:Dynamic, ?msg:String, ?pos:PosInfos):Bool {
+        
+        return _instance.isNot(value, type, msg, pos);
+    }
+    
+    /** Asserts successfully when Reflect.isObject(value) is true. */
+    inline static public function isObject(value:Dynamic, ?msg:String, ?pos:PosInfos):Bool {
+        
+        return _instance.isObject(value, msg, pos);
+    }
+    
+    /**
+     * Asserts successfully when the value parameter is equal to the expected one.
+     * 
+     * @example Assert.equals(10, age);
+     * 
+     * @param expected  The expected value to check against
+     * @param value     The value to test
+     */
+    inline static public function equals(value:Dynamic, expected:Dynamic, ?msg:String, ?pos:PosInfos):Bool {
+        
+        return _instance.equals(value, expected, msg, pos);
+    }
 
-	/**
-	 * Asserts successfully when the value parameter is not the same as the expected one.
-	 * 
-	 * @example Assert.notEquals(10, age);
-	 * 
-	 * @param expected  The expected value to check against
-	 * @param value     The value to test
-	 */
-	inline static public function notEquals(value:Dynamic, expected:Dynamic, ?msg:String, ?pos:PosInfos):Bool {
-		
-		return _instance.notEquals(value, expected, msg, pos);
-	}
+    /**
+     * Asserts successfully when the value parameter is not the same as the expected one.
+     * 
+     * @example Assert.notEquals(10, age);
+     * 
+     * @param expected  The expected value to check against
+     * @param value     The value to test
+     */
+    inline static public function notEquals(value:Dynamic, expected:Dynamic, ?msg:String, ?pos:PosInfos):Bool {
+        
+        return _instance.notEquals(value, expected, msg, pos);
+    }
 
-	/**
-	 * Asserts successfully when the value parameter does match against the passed EReg instance.
-	 * 
-	 * @example Assert.match(~/x/i, "haXe");
-	 * 
-	 * @param pattern  The pattern to match against
-	 * @param value    The value to test
-	 */
-	inline static public function match(pattern:EReg, token:Dynamic, ?msg:String, ?pos:PosInfos):Bool {
-		
-		return _instance.match(pattern, token, msg, pos);
-	}
-	
-	/**
-	 * Same as Assert.equals but considering an approximation error.
-	 * 
-	 * @example Assert.floatEquals(Math.PI, value);
-	 * 
-	 * @param expected  The expected value to check against
-	 * @param value     The value to test
-	 * @param approx    The approximation tollerance. Default is 1e-5
-	 * 
-	 * @todo test the approximation argument
-	 */
-	inline static public function floatEquals(value:Float, expected:Float, approx:Float = 1e-5, ?msg:String, ?pos:PosInfos):Bool {
-		
-		return _instance.floatEquals(value, expected, approx, msg, pos);
-	}
-	
-	/**
-	 * Checks that the test array contains the match parameter.
-	 * @param match   The element that must be included in the tested array
-	 * @param values  The values to test
-	 */
-	inline static public function contains<T>(list:Array<T>, item:T, ?msg:String, ?pos:PosInfos):Bool {
-		
-		return _instance.contains(list, item, msg, pos);
-	}
-	
-	/**
-	 * Checks that the test array does not contain the match parameter.
-	 * @param match   The element that must NOT be included in the tested array
-	 * @param values  The values to test
-	 */
-	inline static public function notContains<T>(list:Array<T>, item:T, ?msg:String, ?pos:PosInfos):Bool {
-		
-		return _instance.notContains(list, item, msg, pos);
-	}
-	
-	/**
-	 * Checks that the expected values is contained in value.
-	 * @param match  The string value that must be contained in value
-	 * @param value  The value to test
-	 */
-	inline static public function stringContains(str:String, token:String, ?msg:String, ?pos:PosInfos):Bool {
-		
-		return _instance.stringContains(str, token, msg, pos);
-	}
+    /**
+     * Asserts successfully when the value parameter does match against the passed EReg instance.
+     * 
+     * @example Assert.match(~/x/i, "haXe");
+     * 
+     * @param pattern  The pattern to match against
+     * @param value    The value to test
+     */
+    inline static public function match(pattern:EReg, token:Dynamic, ?msg:String, ?pos:PosInfos):Bool {
+        
+        return _instance.match(pattern, token, msg, pos);
+    }
+    
+    /**
+     * Same as Assert.equals but considering an approximation error.
+     * 
+     * @example Assert.floatEquals(Math.PI, value);
+     * 
+     * @param expected  The expected value to check against
+     * @param value     The value to test
+     * @param approx    The approximation tollerance. Default is 1e-5
+     */
+    inline static public function floatEquals(value:Float, expected:Float, ?approx:Float, ?msg:String, ?pos:PosInfos):Bool {
+        
+        return _instance.floatEquals(value, expected, approx, msg, pos);
+    }
+    
+    /**
+     * Checks that the test array contains the match parameter.
+     * @param match   The element that must be included in the tested array
+     * @param values  The values to test
+     */
+    inline static public function contains<T>(list:Array<T>, item:T, ?msg:String, ?pos:PosInfos):Bool {
+        
+        return _instance.contains(list, item, msg, pos);
+    }
+    
+    /**
+     * Checks that the test array does not contain the match parameter.
+     * @param match   The element that must NOT be included in the tested array
+     * @param values  The values to test
+     */
+    inline static public function notContains<T>(list:Array<T>, item:T, ?msg:String, ?pos:PosInfos):Bool {
+        
+        return _instance.notContains(list, item, msg, pos);
+    }
+    
+    /**
+     * Checks that the expected values is contained in value.
+     * @param match  The string value that must be contained in value
+     * @param value  The value to test
+     */
+    inline static public function stringContains(str:String, token:String, ?msg:String, ?pos:PosInfos):Bool {
+        
+        return _instance.stringContains(str, token, msg, pos);
+    }
 }
